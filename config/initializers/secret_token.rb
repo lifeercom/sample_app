071289798219168eed66a37dadefad037b5dae3f
lifeercom <lifeercom@hotmail.com>
2012-04-24 16:44:28 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '932657e2035749aa7d4ae327af26fc06c97ce988f326f3a02a48c809bfe9a40a6b987b834821f04908c4bdd35193f07d34f67cadf1d51dabf33dc3e79d32f21e'
